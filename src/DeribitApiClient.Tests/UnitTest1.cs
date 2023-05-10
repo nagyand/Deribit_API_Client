00dea84ba60c8b2bec0f04147035c86474ca63ce
AndrásNagy <sardna.lab@gmail.com>
2023-05-10 16:45:28 +0200

    unit test comments

--- src/DeribitApiClient.Tests/UnitTest1.cs
+++ src/DeribitApiClient.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 namespace DeribitApiClient.Tests;
 
+// For unit testing I would use Moq. Following test should be written:
+// ctor null check, model validation (if there is any), test for every branch, negative test too
+
 public class UnitTest1
 {
     [Fact]
