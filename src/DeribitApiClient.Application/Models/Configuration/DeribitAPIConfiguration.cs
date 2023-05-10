376f343ef72c71f9b622ef6aa2e137af339b77f5
AndrásNagy <sardna.lab@gmail.com>
2023-05-10 15:58:25 +0200

    Add configuration class

---
+++ src/DeribitApiClient.Application/Models/Configuration/DeribitAPIConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DeribitApiClient.Application.Models.Configuration;
+
+public class DeribitAPIConfiguration
+{
+    public string BaseUrl { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public List<string> SubscribeTo { get; set; }
+}
