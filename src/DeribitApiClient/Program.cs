f807d3a0553d5bc7c24a0eb1c68c271b36e90cf6
AndrásNagy <sardna.lab@gmail.com>
2023-05-10 16:40:21 +0200

    GetClient from DI and handle cancel

--- src/DeribitApiClient/Program.cs
+++ src/DeribitApiClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DeribitApiClient.Application.Interfaces;
 using DeribitApiClient.Application.Models.Configuration;
+using DeribitApiClient.Infrastructure.LogProvider;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,10 +21,21 @@ using IHost host = Host.CreateDefaultBuilder(args).ConfigureAppConfiguration
         services.Configure<DeribitApiClientConfig>(hostContext.Configuration.GetSection(nameof(DeribitApiClientConfig)));
         services.AddSingleton<IWebSocketAPIClient, IWebSocketAPIClient>();
         services.AddSingleton<IDeribitAPIHandler, IDeribitAPIHandler>();
+        services.AddSingleton<ILogProvider, LogProvider>();
     }).Build();
 
+CancellationTokenSource cancellationToken = new CancellationTokenSource();
+IDeribitAPIHandler apiHandler = host.Services.GetRequiredService<IDeribitAPIHandler>();
+ILogProvider logger = host.Services.GetRequiredService<ILogProvider>();
 
+await apiHandler.RunAsync(cancellationToken.Token);
 
-
+Console.CancelKeyPress += Console_CancelKeyPress;
+async void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    logger.LogInformation("CTRL+C pressed, initiating graceful shutdown...");
+    cancellationToken.Cancel();
+    e.Cancel = true;
+}
 
 await host.RunAsync();
