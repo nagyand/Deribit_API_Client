f93c5aa434b04d9b57de2397f67192481bda1ce3
AndrásNagy <sardna.lab@gmail.com>
2023-05-10 15:50:10 +0200

    add LogProvider implemantation

---
+++ src/DeribitApiClient.Infrastructure/LogProvider/LogProvider.cs
@@ -0,0 +1,21 @@
+﻿using DeribitApiClient.Application.Interfaces;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+namespace DeribitApiClient.Infrastructure.LogProvider;
+
+/// <summary>
+/// Generic class for logging
+/// </summary>
+public class LogProvider : ILogProvider
+{
+    public void LogError(string errorMessage)
+    {
+        Console.WriteLine($"info [{DateTime.UtcNow}]: {errorMessage}");
+    }
+
+    public void LogInformation(string information)
+    {
+        Console.WriteLine($"info [{DateTime.UtcNow}]: {information}");
+    }
+}
